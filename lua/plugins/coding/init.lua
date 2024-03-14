d47f442216ad319136295407862d22fd257c736a
bruceouyang <bruceouyang@tencent.com>
2024-03-14 11:13:40 +0800

    feat: 添加copilotchat

--- lua/plugins/coding/init.lua
+++ lua/plugins/coding/init.lua
@@ -48,6 +48,55 @@ return {
       require('copilot_cmp').setup()
     end,
   },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    opts = {
+      show_help = 'yes', -- Show help text for CopilotChatInPlace, default: yes
+      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
+      language = 'English', -- Copilot answer language settings when using default prompts. Default language is English.
+      -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
+      -- temperature = 0.1,
+    },
+    build = function()
+      vim.cmd('UpdateRemotePlugins')
+      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+    end,
+    event = 'VeryLazy',
+    keys = {
+      { '<leader>ccc', ':CopilotChat ', desc = 'CopilotChat - Chat start' },
+      { '<leader>ccm', '<cmd>CopilotChat 是什么意思<cr>', desc = 'CopilotChat - code experimental' },
+      { '<leader>ccb', ':CopilotChatBuffer ', desc = 'CopilotChat - Chat with current buffer' },
+      { '<leader>cce', '<cmd>CopilotChatExplain<cr>', desc = 'CopilotChat - Explain code' },
+      { '<leader>cct', '<cmd>CopilotChatTests<cr>', desc = 'CopilotChat - Generate tests' },
+      {
+        '<leader>ccn',
+        '<cmd>CopilotChat 给这个变量命名<cr>',
+        desc = 'CopilotChat - Variable naming',
+      },
+      {
+        '<leader>ccf',
+        '<cmd>CopilotChat 给这个方法命名<cr>',
+        desc = 'CopilotChat - Function naming',
+      },
+      {
+        '<leader>ccT',
+        '<cmd>CopilotChatVsplitToggle<cr>',
+        desc = 'CopilotChat - Toggle Vsplit', -- Toggle vertical split
+      },
+      {
+        '<leader>ccx',
+        ':CopilotChatInPlace<cr>',
+        mode = 'x',
+        desc = 'CopilotChat - Run in-place code',
+      },
+      {
+        '<leader>ccr',
+        '<cmd>CopilotChatReset<cr>', -- Reset chat history and clear buffer.
+        desc = 'CopilotChat - Reset chat history and clear buffer',
+      },
+    },
+  },
   -- auto completion
   {
     'hrsh7th/nvim-cmp',
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -279,13 +279,4 @@ return {
       end
     end,
   },
-  {
-    'Wansmer/symbol-usage.nvim',
-    event = 'LspAttach', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-    config = function()
-      require('symbol-usage').setup({
-        implementation = { enabled = true },
-      })
-    end,
-  },
 }
