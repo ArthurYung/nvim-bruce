0c9f1def8218ffc4a4183a8e1458cc446d81c247
bruceouyang <bruceouyang@tencent.com>
2025-02-06 17:09:25 +0800

    feat: 修改file selector驱动

--- lua/plugins/coding/init.lua
+++ lua/plugins/coding/init.lua
@@ -38,12 +38,15 @@ return {
       auto_suggestions_provider = 'openai', -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
       openai = {
         endpoint = 'https://api.deepseek.com/v1',
-        model = 'deepseek-chat',
+        model = 'deepseek-reasoner',
         timeout = 30000, -- Timeout in milliseconds
         temperature = 0,
         max_tokens = 4096,
         -- ['local'] = false,
         api_key_name = 'DEEPSEEK_API_KEY',
+      },
+      file_selector = {
+        provider = 'telescope',
       },
       --- @class AvanteConflictUserConfig
       diff = {
